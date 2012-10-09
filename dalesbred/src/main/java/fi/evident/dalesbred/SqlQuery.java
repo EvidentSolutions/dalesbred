55087768e7fae2961c80958456cb6489b91b4364
Juha Komulainen <juha.komulainen@evident.fi>
2012-10-09 10:22:47 +0300

    Added getters and nullability annotations.

--- dalesbred/src/main/java/fi/evident/dalesbred/SqlQuery.java
+++ dalesbred/src/main/java/fi/evident/dalesbred/SqlQuery.java
@@ -1,24 +1,39 @@
 package fi.evident.dalesbred;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Iterator;
 import java.util.List;
 
 import static fi.evident.dalesbred.utils.Require.requireNonNull;
 import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableList;
 
 public final class SqlQuery {
     final String sql;
     final List<Object> args;
 
-    private SqlQuery(@SQL String sql, Object... args) {
+    private SqlQuery(@NotNull @SQL String sql, Object... args) {
         this.sql = requireNonNull(sql);
-        this.args = asList(args);
+        this.args = unmodifiableList(asList(args));
     }
 
-    public static SqlQuery query(@SQL String sql, Object... args) {
+    @NotNull
+    public static SqlQuery query(@NotNull @SQL String sql, Object... args) {
         return new SqlQuery(sql, args);
     }
 
+    @NotNull
+    public String getSql() {
+        return sql;
+    }
+
+    @NotNull
+    public List<Object> getArguments() {
+        return args;
+    }
+
+    @NotNull
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
