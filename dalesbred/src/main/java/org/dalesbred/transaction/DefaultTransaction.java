31c5da191b18471e6c70d8d29cc59c44b1bf5082
Juha Komulainen <juha.komulainen@evident.fi>
2016-02-12 22:59:42 +0200

    Restore semantics of logging

--- dalesbred/src/main/java/org/dalesbred/transaction/DefaultTransaction.java
+++ dalesbred/src/main/java/org/dalesbred/transaction/DefaultTransaction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Evident Solutions Oy
+ * Copyright (c) 2016 Evident Solutions Oy
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -59,7 +59,7 @@ final class DefaultTransaction {
 
             } catch (Exception e) {
                 connection.rollback();
-                log.warn("rolled back transaction because of exception.", e);
+                log.warn("rolled back transaction because of exception: " + e, e);
                 throw Throwables.propagate(e, SQLException.class);
             }
         } catch (SQLException e) {
@@ -81,7 +81,7 @@ final class DefaultTransaction {
 
             } catch (Exception e) {
                 connection.rollback(savepoint);
-                log.warn("rolled back nested transaction because of exception.", e);
+                log.warn("rolled back nested transaction because of exception: " + e, e);
                 throw Throwables.propagate(e, SQLException.class);
             }
         } catch (SQLException e) {
