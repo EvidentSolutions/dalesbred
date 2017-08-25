8792214a5574e5ff11957863f07da359afe81b60
Panu Akerman <panu.akerman@evident.fi>
2017-08-25 14:01:13 +0300

    Add documentation

--- dalesbred/src/main/java/org/dalesbred/query/FetchDirection.java
+++ dalesbred/src/main/java/org/dalesbred/query/FetchDirection.java
@@ -24,6 +24,11 @@ package org.dalesbred.query;
 
 import java.sql.ResultSet;
 
+/**
+ * Represents fetch direction that can be given as a hint to {@link SqlQuery#setFetchDirection(FetchDirection)}.
+ *
+ * @see java.sql.PreparedStatement#setFetchDirection(int)
+ */
 public enum FetchDirection {
     FORWARD(ResultSet.FETCH_FORWARD),
     REVERSE(ResultSet.FETCH_REVERSE),
