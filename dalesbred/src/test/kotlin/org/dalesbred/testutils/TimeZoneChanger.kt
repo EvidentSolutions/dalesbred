21895bd4edf6b058b81a2e1b1ef3642fa1250e5e
Juha Komulainen <juha.komulainen@evident.fi>
2018-06-13 23:59:22 +0300

    Generalize time zone changer

--- dalesbred/src/test/kotlin/org/dalesbred/testutils/TimeZoneChanger.kt
+++ dalesbred/src/test/kotlin/org/dalesbred/testutils/TimeZoneChanger.kt
@@ -26,9 +26,13 @@ import org.joda.time.DateTimeZone
 import java.util.*
 
 inline fun withUTCTimeZone(block: () -> Unit) {
+    withTimeZone("UTC", block)
+}
+
+inline fun withTimeZone(name: String, block: () -> Unit) {
     val old = TimeZone.getDefault()
     try {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+        TimeZone.setDefault(TimeZone.getTimeZone(name))
         block()
     } finally {
         TimeZone.setDefault(old)
