d42b6741f50a01d4d14ff67bb61ac97815f7097e
Juha Komulainen <juha.komulainen@evident.fi>
2021-11-30 13:54:46 +0200

    Add build scan agreement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,14 @@ rootProject.name = "dalesbred-root"
 include(":dalesbred")
 include(":dalesbred-junit")
 include(":website")
+
+plugins {
+    id("com.gradle.enterprise") version("3.7.2")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
