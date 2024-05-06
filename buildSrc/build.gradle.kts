c559e9207c129ca67a6cdab0cf9416a9584df97e
Juha Komulainen <juha.komulainen@evident.fi>
2024-05-06 10:19:46 +0300

    Update Kotlin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    val kotlinVersion = "1.5.31"
+    val kotlinVersion = "1.9.23"
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
     implementation("org.asciidoctor:asciidoctor-gradle-jvm:3.3.2")
