eeac91b1ba1d158ec400d35c5969dd851336b29b
Juha Komulainen <juha.komulainen@evident.fi>
2025-07-12 20:01:10 +0300

    Build using JDK 11 since deployment plugin requires it

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,1 +1,19 @@
+dependencyResolutionManagement {
 
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+
+    versionCatalogs {
+//        create("libs") {
+//            from(files("../gradle/libs.versions.toml"))
+//        }
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "buildSrc"
