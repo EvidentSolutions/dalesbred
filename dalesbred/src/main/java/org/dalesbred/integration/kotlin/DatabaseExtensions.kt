32c833e252c1fb66baeaa0f28222713a31fa6d39
Juha Komulainen <juha.komulainen@evident.fi>
2026-08-04 10:34:13 +0300

    Generalize Kotlin extensions to support DatabaseAccess

--- dalesbred/src/main/java/org/dalesbred/integration/kotlin/DatabaseExtensions.kt
+++ dalesbred/src/main/java/org/dalesbred/integration/kotlin/DatabaseExtensions.kt
@@ -1,6 +1,6 @@
 package org.dalesbred.integration.kotlin
 
-import org.dalesbred.Database
+import org.dalesbred.DatabaseAccess
 import org.dalesbred.query.SqlQuery
 import org.dalesbred.result.EmptyResultException
 import org.dalesbred.result.NonUniqueResultException
@@ -11,13 +11,13 @@ import java.util.*
 /**
  * Executes a query and converts the results to instances of given class using default mechanisms.
  */
-inline fun <reified T : Any> Database.findAll(query: SqlQuery): List<T> =
+inline fun <reified T : Any> DatabaseAccess.findAll(query: SqlQuery): List<T> =
     findAll(T::class.java, query)
 
 /**
  * Executes a query and converts the results to instances of given class using default mechanisms.
  */
-inline fun <reified T : Any> Database.findAll(@Language("SQL") sql: String, vararg args: Any?): List<T> =
+inline fun <reified T : Any> DatabaseAccess.findAll(@Language("SQL") sql: String, vararg args: Any?): List<T> =
     findAll(SqlQuery.query(sql, *args))
 
 /**
@@ -26,7 +26,7 @@ inline fun <reified T : Any> Database.findAll(@Language("SQL") sql: String, vara
  * @throws NonUniqueResultException if there is more then one row
  * @throws EmptyResultException if there are no rows
  */
-inline fun <reified T : Any> Database.findUnique(query: SqlQuery): T =
+inline fun <reified T : Any> DatabaseAccess.findUnique(query: SqlQuery): T =
     findUnique(T::class.java, query)
 
 /**
@@ -35,66 +35,66 @@ inline fun <reified T : Any> Database.findUnique(query: SqlQuery): T =
  * @throws NonUniqueResultException if there is more then one row
  * @throws EmptyResultException if there are no rows
  */
-inline fun <reified T : Any> Database.findUnique(@Language("SQL") sql: String, vararg args: Any?): T =
+inline fun <reified T : Any> DatabaseAccess.findUnique(@Language("SQL") sql: String, vararg args: Any?): T =
     findUnique(SqlQuery.query(sql, *args))
 
-inline fun <reified T : Any> Database.findUniqueOrNull(query: SqlQuery): T? =
+inline fun <reified T : Any> DatabaseAccess.findUniqueOrNull(query: SqlQuery): T? =
     findUniqueOrNull(T::class.java, query)
 
-inline fun <reified T : Any> Database.findUniqueOrNull(@Language("SQL") sql: String, vararg args: Any?): T? =
+inline fun <reified T : Any> DatabaseAccess.findUniqueOrNull(@Language("SQL") sql: String, vararg args: Any?): T? =
     findUniqueOrNull(SqlQuery.query(sql, *args))
 
-inline fun <reified T : Any> Database.findOptional(query: SqlQuery): Optional<T> =
+inline fun <reified T : Any> DatabaseAccess.findOptional(query: SqlQuery): Optional<T> =
     findOptional(T::class.java, query)
 
-inline fun <reified T : Any> Database.findOptional(@Language("SQL") sql: String, vararg args: Any?): Optional<T> =
+inline fun <reified T : Any> DatabaseAccess.findOptional(@Language("SQL") sql: String, vararg args: Any?): Optional<T> =
     findOptional(SqlQuery.query(sql, *args))
 
-inline fun <reified K : Any, reified V : Any> Database.findMap(query: SqlQuery): Map<K, V> =
+inline fun <reified K : Any, reified V : Any> DatabaseAccess.findMap(query: SqlQuery): Map<K, V> =
     findMap(K::class.java, V::class.java, query)
 
-inline fun <reified K : Any, reified V : Any> Database.findMap(
+inline fun <reified K : Any, reified V : Any> DatabaseAccess.findMap(
     @Language("SQL") sql: String,
     vararg args: Any?
 ): Map<K, V> =
     findMap(SqlQuery.query(sql, *args))
 
-fun <T : Any> Database.executeQuery(query: SqlQuery, resultSetProcessor: (ResultSet) -> T): T =
+fun <T : Any> DatabaseAccess.executeQuery(query: SqlQuery, resultSetProcessor: (ResultSet) -> T): T =
     executeQuery({ resultSetProcessor(it) }, query)
 
-fun <T : Any> Database.executeQuery(
+fun <T : Any> DatabaseAccess.executeQuery(
     @Language("SQL") sql: String,
     vararg args: Any?,
     resultSetProcessor: (ResultSet) -> T
 ): T =
     executeQuery(SqlQuery.query(sql, *args), resultSetProcessor)
 
-fun <T : Any> Database.findAll(query: SqlQuery, rowMapper: (ResultSet) -> T): List<T> =
+fun <T : Any> DatabaseAccess.findAll(query: SqlQuery, rowMapper: (ResultSet) -> T): List<T> =
     findAll({ rowMapper(it) }, query)
 
-fun <T : Any> Database.findAll(@Language("SQL") sql: String, vararg args: Any?, rowMapper: (ResultSet) -> T): List<T> =
+fun <T : Any> DatabaseAccess.findAll(@Language("SQL") sql: String, vararg args: Any?, rowMapper: (ResultSet) -> T): List<T> =
     findAll(SqlQuery.query(sql, *args), rowMapper)
 
-fun <T : Any> Database.findUnique(query: SqlQuery, rowMapper: (ResultSet) -> T): T =
+fun <T : Any> DatabaseAccess.findUnique(query: SqlQuery, rowMapper: (ResultSet) -> T): T =
     findUnique({ rowMapper(it) }, query)
 
-fun <T : Any> Database.findUnique(@Language("SQL") sql: String, vararg args: Any?, rowMapper: (ResultSet) -> T): T =
+fun <T : Any> DatabaseAccess.findUnique(@Language("SQL") sql: String, vararg args: Any?, rowMapper: (ResultSet) -> T): T =
     findUnique(SqlQuery.query(sql, *args), rowMapper)
 
-fun <T : Any> Database.findUniqueOrNull(query: SqlQuery, rowMapper: (ResultSet) -> T): T? =
+fun <T : Any> DatabaseAccess.findUniqueOrNull(query: SqlQuery, rowMapper: (ResultSet) -> T): T? =
     findUniqueOrNull({ rowMapper(it) }, query)
 
-fun <T : Any> Database.findUniqueOrNull(
+fun <T : Any> DatabaseAccess.findUniqueOrNull(
     @Language("SQL") sql: String,
     vararg args: Any?,
     rowMapper: (ResultSet) -> T
 ): T? =
     findUniqueOrNull(SqlQuery.query(sql, *args), rowMapper)
 
-fun <T : Any> Database.findOptional(query: SqlQuery, rowMapper: (ResultSet) -> T): Optional<T> =
+fun <T : Any> DatabaseAccess.findOptional(query: SqlQuery, rowMapper: (ResultSet) -> T): Optional<T> =
     findOptional({ rowMapper(it) }, query)
 
-fun <T : Any> Database.findOptional(
+fun <T : Any> DatabaseAccess.findOptional(
     @Language("SQL") sql: String,
     vararg args: Any?,
     rowMapper: (ResultSet) -> T
