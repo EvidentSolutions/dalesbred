ec78ab2eb0cd78eeae2480c4ff642a6778dd2669
Juha Komulainen <juha.komulainen@evident.fi>
2026-08-04 11:32:39 +0300

    Add a workaround for failing javadoc build

---
+++ dalesbred/src/main/java/org/dalesbred/integration/kotlin/Dummy.java
@@ -0,0 +1,6 @@
+package org.dalesbred.integration.kotlin;
+
+// Without this class, Javadoc fails compilation as the package is references from module-info,
+// but has no Java classes at ll.
+@SuppressWarnings("unused")
+final class Dummy { }
